0b9042eb9c48f6b2116cb67377e0ea455076c1d6
Susie Riley <susie09rob@gmail.com>
2022-01-19 15:05:39 -0800

    Second Commit

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,30 +2,33 @@ import React, { useReducer } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
+//Action objects { colorToChange: 'red', amountL 15 }
+//Convention { type: 'change_red', payload: 15 }
 const COLOR_INCREMENT = 15;
-
 //Reducer
 //A hook function is similar to how usedState is a hook function as well.
 //a hook adds in additional functionality or some additional capability to a functional component.
-//So we're going to use use reducer to add in the functionality to essentially use a reducer inside of our square screen.
+//Use useReducer to add in the functionality to essentially use a reducer inside of our square screen.
 const reducer = (state, action) => {
 	// state === {red: number, green: number, blue: number}
 	// action === {colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15}
+	//OR convention action === { type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15 }
 
-	switch (action.colorToChange) {
-		case 'red':
-			return state.red + action.amount > 255 || state.red + action.amount < 0
+	switch (action.type) {
+		case 'change_red':
+			return state.red + action.payload > 255 || state.red + action.payload < 0
 				? state
-				: { ...state, red: state.red + action.amount };
-		case 'green':
-			return state.green + action.amount > 255 ||
-				state.green + action.amount < 0
+				: { ...state, red: state.red + action.payload };
+		case 'change_green':
+			return state.green + action.payload > 255 ||
+				state.green + action.payload < 0
 				? state
-				: { ...state, green: state.green + action.amount };
-		case 'blue':
-			return state.blue + action.amount > 255 || state.blue + action.amount < 0
+				: { ...state, green: state.green + action.payload };
+		case 'change_blue':
+			return state.blue + action.payload > 255 ||
+				state.blue + action.payload < 0
 				? state
-				: { ...state, blue: state.blue + action.amount };
+				: { ...state, blue: state.blue + action.payload };
 		default:
 			return state;
 	}
@@ -39,28 +42,28 @@ const SquareScreen = () => {
 		<View>
 			<ColorCounter
 				onIncrease={() =>
-					dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })
+					dispatch({ type: 'change_red', payload: COLOR_INCREMENT })
 				}
 				onDecrease={() =>
-					dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })
+					dispatch({ type: 'change_red', payload: -1 * COLOR_INCREMENT })
 				}
 				color="Red"
 			/>
 			<ColorCounter
 				onIncrease={() =>
-					dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })
+					dispatch({ type: 'change_blue', payload: COLOR_INCREMENT })
 				}
 				onDecrease={() =>
-					dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })
+					dispatch({ type: 'change_blue', payload: -1 * COLOR_INCREMENT })
 				}
 				color="Blue"
 			/>
 			<ColorCounter
 				onIncrease={() =>
-					dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })
+					dispatch({ type: 'change_green', payload: COLOR_INCREMENT })
 				}
 				onDecrease={() =>
-					dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })
+					dispatch({ type: 'change_green', payload: -1 * COLOR_INCREMENT })
 				}
 				color="Green"
 			/>
