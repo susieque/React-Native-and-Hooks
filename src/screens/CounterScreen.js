7b4b19408183aeaacfcd57e4b1a5e59aeaecee50
Susie Riley <susie09rob@gmail.com>
2022-01-19 15:30:01 -0800

    Third Commit

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,28 +1,37 @@
-// import useState(refered to as a hook) A hook is a function that adds some new functionality to a function component. We are using in the CounterScreen function to make a special kind of variable that React is going to watch and update it's value, automatically update our component on the screen. useState is going to initalize our piece of state... const [counter, setCounter] = useState(0);  as opposed to doing a normal variable declaration. useState to initialze a new piece of state or new piece of data that we're going to track. [] is array destructuring another way of pulling some elements or a reference to elements out of an array.
-
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const reducer = (state, action) => {
+	//state === put object in curly brackets { count: number }
+	//action === put in type property in curly brackets thats a string describing the change we need to make to our state object. { type: 'increment' || 'decrement', payload: 1 }
+	switch (action.type) {
+		case 'increment':
+			return { ...state, count: state.count + action.payload };
+		case 'decrement':
+			return { ...state, count: state.count - action.payload };
+		default:
+			return state;
+	}
+};
+
 const CounterScreen = () => {
-	const [counter, setCounter] = useState(0);
+	const [state, dispatch] = useReducer(reducer, { count: 0 });
 
 	return (
 		<View>
 			<Button
 				title="increase"
 				onPress={() => {
-					//Don't do this, we don't modify a state variable directly. We use setCounter. Anytime we call setCounter function React automatically reruns the entire function component we created.
-					// counter++;
-					setCounter(counter + 1);
+					dispatch({ type: 'increment', payload: 1 });
 				}}
 			/>
 			<Button
 				title="decrease"
 				onPress={() => {
-					setCounter(counter - 1);
+					dispatch({ type: 'decrement', payload: 1 });
 				}}
 			/>
-			<Text>Current Count: {counter} </Text>
+			<Text>Current Count: {state.count} </Text>
 		</View>
 	);
 };
